73dc94ac2e14e07b11cde5b4a8bdb3114b1445ee
Chrisss42 <chris_green42@icloud.com>
2016-03-23 10:31:12 -0400

    23/03/2016 10:30
    Chris

---
+++ Whitecollarlegit/Form3.vb
@@ -0,0 +1,66 @@
+﻿Public Class Level2
+    Public letters(25) As Button
+    Dim accept As Integer
+    Dim space As Button
+    Dim buttonpoint As Point
+    Dim qwerty As String = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    Dim myfont As Font = New Font(FontFamily.GenericSansSerif, 14.0F, FontStyle.Bold)
+    Dim wordnum As Integer
+    Dim guessletter As String
+    Dim guesses As String
+    Dim Words(5) As String
+    Dim correct As Boolean
+    Dim word As String
+    Dim letterpos As Integer
+    Private Sub Level2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        'accept = MsgBox("Accept?", 4, "Accept")
+        'If accept = 6 Then
+        '    Me.Show()
+        'Else
+        '    End
+        'End If
+
+
+        buttonpoint.X = 535
+        buttonpoint.Y = 65
+        'Define buttons in button array
+        For x = 0 To 25
+            letters(x) = New Button
+            letters(x).Width = 35
+            letters(x).Height = 35
+            letters(x).Location = buttonpoint
+            buttonpoint.X = buttonpoint.X + 40
+            letters(x).Text = Mid(qwerty, (x + 1), 1)
+            letters(x).BackColor = Color.DodgerBlue
+            letters(x).ForeColor = Color.White
+            letters(x).FlatStyle = FlatStyle.Flat
+            letters(x).Font = myfont
+            If x = 9 Then
+                buttonpoint.Y = 105
+                buttonpoint.X = 555
+            End If
+            If x = 18 Then
+                buttonpoint.Y = 145
+                buttonpoint.X = 575
+            End If
+            AddHandler letters(x).Click, AddressOf Clicked
+            Me.Controls.Add(letters(x))
+        Next
+        buttonpoint.X = 655
+        buttonpoint.Y = 185
+        Space = New Button
+        Space.Width = 195
+        Space.Height = 35
+        Space.Location = buttonpoint
+        Space.BackColor = Color.DodgerBlue
+        Space.ForeColor = Color.White
+        Space.FlatStyle = FlatStyle.Flat
+        Space.Font = myfont
+        Space.Text = "Space"
+        AddHandler Space.Click, AddressOf Clicked
+        Me.Controls.Add(Space)
+        genWord()
+    End Sub
+End Class
--- Whitecollarlegit/Level1.Designer.vb
+++ Whitecollarlegit/Level1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Level1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Level1))
         Me.lblPoint = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -107,6 +108,7 @@ Partial Class Level1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(943, 409)
         Me.Controls.Add(Me.lblPoint)
         Me.Controls.Add(Me.Label4)
